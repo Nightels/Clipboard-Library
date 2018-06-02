41ff8758f28847f3f2d8c1ce579a92057b249888
Roland <observerkovrol@gmail.com>
2018-06-02 14:09:36 +0200

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,361 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ShowProgramToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HideProgramToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.HistoryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NoHistoryInClipboardToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.IfYouNeedMoreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator5 = New System.Windows.Forms.ToolStripSeparator()
+        Me.EnableProgramToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ContextMenuStrip2 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.OpenInBrowserToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.RemoveFromListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.ContextMenuStrip2.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "ClipLib - Small GUI" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Version : 4.2" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Free Clipboard Manager"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowProgramToolStripMenuItem, Me.HideProgramToolStripMenuItem, Me.ToolStripSeparator3, Me.HistoryToolStripMenuItem, Me.ToolStripSeparator5, Me.EnableProgramToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(177, 104)
+        '
+        'ShowProgramToolStripMenuItem
+        '
+        Me.ShowProgramToolStripMenuItem.Name = "ShowProgramToolStripMenuItem"
+        Me.ShowProgramToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.ShowProgramToolStripMenuItem.Text = "Show Program..."
+        '
+        'HideProgramToolStripMenuItem
+        '
+        Me.HideProgramToolStripMenuItem.Name = "HideProgramToolStripMenuItem"
+        Me.HideProgramToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.HideProgramToolStripMenuItem.Text = "Hide Program..."
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(173, 6)
+        '
+        'HistoryToolStripMenuItem
+        '
+        Me.HistoryToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NoHistoryInClipboardToolStripMenuItem, Me.ToolStripSeparator4, Me.IfYouNeedMoreToolStripMenuItem})
+        Me.HistoryToolStripMenuItem.Name = "HistoryToolStripMenuItem"
+        Me.HistoryToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.HistoryToolStripMenuItem.Text = "Now In Clipboard..."
+        '
+        'NoHistoryInClipboardToolStripMenuItem
+        '
+        Me.NoHistoryInClipboardToolStripMenuItem.Name = "NoHistoryInClipboardToolStripMenuItem"
+        Me.NoHistoryInClipboardToolStripMenuItem.Size = New System.Drawing.Size(223, 22)
+        Me.NoHistoryInClipboardToolStripMenuItem.Text = "No History In Clipboard"
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(220, 6)
+        '
+        'IfYouNeedMoreToolStripMenuItem
+        '
+        Me.IfYouNeedMoreToolStripMenuItem.Name = "IfYouNeedMoreToolStripMenuItem"
+        Me.IfYouNeedMoreToolStripMenuItem.Size = New System.Drawing.Size(223, 22)
+        Me.IfYouNeedMoreToolStripMenuItem.Text = "If you need more, click here!"
+        '
+        'ToolStripSeparator5
+        '
+        Me.ToolStripSeparator5.Name = "ToolStripSeparator5"
+        Me.ToolStripSeparator5.Size = New System.Drawing.Size(173, 6)
+        Me.ToolStripSeparator5.Visible = False
+        '
+        'EnableProgramToolStripMenuItem
+        '
+        Me.EnableProgramToolStripMenuItem.Name = "EnableProgramToolStripMenuItem"
+        Me.EnableProgramToolStripMenuItem.Size = New System.Drawing.Size(176, 22)
+        Me.EnableProgramToolStripMenuItem.Text = "Enable Program..."
+        Me.EnableProgramToolStripMenuItem.Visible = False
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(350, 197)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.ListBox1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(342, 171)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "History"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(232, 11)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Visible = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListBox1.ContextMenuStrip = Me.ContextMenuStrip2
+        Me.ListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.Location = New System.Drawing.Point(6, 6)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(330, 158)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ContextMenuStrip2
+        '
+        Me.ContextMenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem, Me.ToolStripSeparator1, Me.OpenInBrowserToolStripMenuItem, Me.ToolStripSeparator2, Me.RemoveFromListToolStripMenuItem})
+        Me.ContextMenuStrip2.Name = "ContextMenuStrip2"
+        Me.ContextMenuStrip2.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+        Me.ContextMenuStrip2.Size = New System.Drawing.Size(174, 82)
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(173, 22)
+        Me.CopyToolStripMenuItem.Text = "Copy"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(170, 6)
+        '
+        'OpenInBrowserToolStripMenuItem
+        '
+        Me.OpenInBrowserToolStripMenuItem.Name = "OpenInBrowserToolStripMenuItem"
+        Me.OpenInBrowserToolStripMenuItem.Size = New System.Drawing.Size(173, 22)
+        Me.OpenInBrowserToolStripMenuItem.Text = "Open In Browser..."
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(170, 6)
+        '
+        'RemoveFromListToolStripMenuItem
+        '
+        Me.RemoveFromListToolStripMenuItem.Name = "RemoveFromListToolStripMenuItem"
+        Me.RemoveFromListToolStripMenuItem.Size = New System.Drawing.Size(173, 22)
+        Me.RemoveFromListToolStripMenuItem.Text = "Remove from list..."
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.GroupBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(342, 171)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Options"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox7)
+        Me.GroupBox1.Controls.Add(Me.CheckBox6)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.CheckBox5)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(330, 159)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Options"
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.AutoSize = True
+        Me.CheckBox7.Checked = True
+        Me.CheckBox7.CheckState = System.Windows.Forms.CheckState.Indeterminate
+        Me.CheckBox7.Enabled = False
+        Me.CheckBox7.Location = New System.Drawing.Point(6, 136)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(129, 17)
+        Me.CheckBox7.TabIndex = 7
+        Me.CheckBox7.Text = "More options shortly..."
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Checked = True
+        Me.CheckBox6.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox6.Location = New System.Drawing.Point(6, 87)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(113, 17)
+        Me.CheckBox6.TabIndex = 6
+        Me.CheckBox6.Text = "Use the program..."
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(6, 20)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(59, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Enable"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Checked = True
+        Me.CheckBox5.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox5.Location = New System.Drawing.Point(6, 64)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(117, 17)
+        Me.CheckBox5.TabIndex = 5
+        Me.CheckBox5.Text = "Start with Windows"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(6, 43)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(109, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Enable Small GUI"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 194)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ClipboardLibrary"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.ContextMenuStrip2.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ContextMenuStrip2 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents CopyToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents OpenInBrowserToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RemoveFromListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ColorDialog1 As System.Windows.Forms.ColorDialog
+    Friend WithEvents CheckBox7 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox6 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox5 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents ShowProgramToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HideProgramToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator3 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents HistoryToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NoHistoryInClipboardToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator4 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents IfYouNeedMoreToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ToolStripSeparator5 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents EnableProgramToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,159 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add(My.Computer.Clipboard.GetText)
+        NoHistoryInClipboardToolStripMenuItem.Text = My.Computer.Clipboard.GetText
+        TextBox1.Text = My.Computer.Clipboard.GetText
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        TextBox1.Text = My.Computer.Clipboard.GetText
+        If Me.Visible = True Then
+            ShowProgramToolStripMenuItem.Enabled = False
+            HideProgramToolStripMenuItem.Enabled = True
+        End If
+        If Me.Visible = False Then
+            ShowProgramToolStripMenuItem.Enabled = True
+            HideProgramToolStripMenuItem.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        If TextBox1.Text = My.Computer.Clipboard.GetText Then
+
+        Else
+            ListBox1.Items.Add(My.Computer.Clipboard.GetText)
+            NoHistoryInClipboardToolStripMenuItem.Text = My.Computer.Clipboard.GetText
+            TextBox1.Text = My.Computer.Clipboard.GetText
+        End If
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CopyToolStripMenuItem.Click
+        Dim copy_buffer As New System.Text.StringBuilder
+        For Each item As Object In ListBox1.SelectedItems
+            copy_buffer.AppendLine(item.ToString)
+        Next
+        If copy_buffer.Length > 0 Then
+            Clipboard.SetText(copy_buffer.ToString)
+        End If
+    End Sub
+
+    Private Sub OpenInBrowserToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OpenInBrowserToolStripMenuItem.Click
+        Dim copy_buffer As New System.Text.StringBuilder
+        For Each item As Object In ListBox1.SelectedItems
+            copy_buffer.AppendLine(item.ToString)
+        Next
+        If copy_buffer.Length > 0 Then
+            Process.Start("https://www.google.com/search?q=" & copy_buffer.ToString)
+        End If
+    End Sub
+
+    Private Sub RemoveFromListToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RemoveFromListToolStripMenuItem.Click
+        ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+    End Sub
+
+    Private Sub ListBox1_DrawItem(ByVal sender As Object, ByVal e As System.Windows.Forms.DrawItemEventArgs) Handles ListBox1.DrawItem
+        e.DrawBackground()
+        If (e.State And DrawItemState.Selected) = DrawItemState.Selected Then
+            e.Graphics.FillRectangle(Brushes.Blue, e.Bounds)
+        End If
+        Using b As New SolidBrush(e.ForeColor)
+            e.Graphics.DrawString(ListBox1.GetItemText(ListBox1.Items(e.Index)), e.Font, b, e.Bounds)
+        End Using
+        e.DrawFocusRectangle()
+    End Sub
+
+    Private Sub ChangeToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Timer1.Start()
+            Timer2.Start()
+            CheckBox1.Text = "Enabled"
+        End If
+        If CheckBox1.Checked = False Then
+            Timer1.Stop()
+            Timer2.Stop()
+            MsgBox("Disabled the program. Please click again to enable!", MsgBoxStyle.Exclamation, "Exclamation")
+            CheckBox1.Text = "Disabled"
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            NotifyIcon1.Visible = True
+        End If
+        If CheckBox2.Checked = False Then
+            NotifyIcon1.Visible = False
+        End If
+    End Sub
+
+    Private Sub ShowProgramToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ShowProgramToolStripMenuItem.Click
+        Me.Show()
+        Me.WindowState = System.Windows.Forms.FormWindowState.Normal
+    End Sub
+
+    Private Sub HideProgramToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HideProgramToolStripMenuItem.Click
+        Me.Hide()
+        MsgBox("The program is hide, don't closed!", MsgBoxStyle.Information, "Information")
+    End Sub
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        If (MessageBox.Show("You are close the application?", "Close?", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.No) Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox5.CheckedChanged
+        Dim applicationName As String = Application.ProductName
+        Dim applicationPath As String = Application.ExecutablePath
+
+        If CheckBox5.Checked = True Then
+            Dim regKey As Microsoft.Win32.RegistryKey
+            regKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+            regKey.SetValue(applicationName, """" & applicationPath & """")
+            regKey.Close()
+        End If
+        If CheckBox5.Checked = False Then
+            Dim regKey As Microsoft.Win32.RegistryKey
+            regKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+            regKey.DeleteValue(applicationName, False)
+            regKey.Close()
+        End If
+    End Sub
+
+    Private Sub CheckBox6_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox6.CheckedChanged
+        If CheckBox6.Checked = True Then
+            CheckBox6.Text = "Use the program..."
+        End If
+        If CheckBox6.Checked = False Then
+            If CheckBox2.Checked = True Then
+                Me.Hide()
+                CheckBox6.Text = "Use the GUI..."
+                EnableProgramToolStripMenuItem.Visible = True
+                ToolStripSeparator5.Visible = True
+                MsgBox("The program hide, you use small GUI. If you use a program, click 'Enable Program' on small GUI!", MsgBoxStyle.Information, "Yes?")
+            Else
+                MsgBox("You don't enable 'small GUI'!", MsgBoxStyle.Exclamation, "Ohh Error")
+                CheckBox6.Checked = True
+            End If
+        End If
+    End Sub
+
+    Private Sub IfYouNeedMoreToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles IfYouNeedMoreToolStripMenuItem.Click
+        Me.Show()
+        Me.WindowState = System.Windows.Forms.FormWindowState.Normal
+    End Sub
+
+    Private Sub EnableProgramToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnableProgramToolStripMenuItem.Click
+        Me.Show()
+        EnableProgramToolStripMenuItem.Visible = False
+        ToolStripSeparator5.Visible = False
+        CheckBox6.Checked = True
+        MsgBox("You are activated a program!", MsgBoxStyle.Information, "Ohh")
+    End Sub
+    Private Sub NoHistoryInClipboardToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NoHistoryInClipboardToolStripMenuItem.Click
+        Clipboard.SetText(NoHistoryInClipboardToolStripMenuItem.Text)
+    End Sub
+End Class
